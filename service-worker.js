58205bc36cab127a9de4ddf62f19931b6a3486cd
dfluch-hub <d.fluch@gmx.at>
2026-09-07 15:39:17 +0200

    Update PWA cache for Smoke Lab V19

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='smoke-change-lab-premium-v2-20260905';
+const CACHE='smoke-change-lab-premium-v19-20260907';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
