9f09588c1b74c091aff963f8c7a9d046817fb0a7
dfluch-hub <d.fluch@gmx.at>
2026-09-07 16:13:31 +0200

    Refresh PWA cache for Smoke Lab UX theme

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='smoke-change-lab-premium-v23-20260907';
+const CACHE='smoke-change-lab-premium-v24-20260907';
 const ASSETS=['./','./index.html','./app.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
